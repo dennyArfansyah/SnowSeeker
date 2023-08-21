838ce17dc418bd89a17dc4647973ab07c6da1a9a
dennyArfansyah <dennyarfansyah@gmail.com>
2023-08-21 14:59:48 +0700

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Denny Arfansyah on 21/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
